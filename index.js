a2e8a30183b99b2f68eb8b9c0fc74903a3d4b1e8
paullmich28 <paullmich28@gmail.com>
2023-12-30 14:53:00 +0700

    2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
-const URL = "https://api.openbrewerydb.org/v1/breweries/random";
+const randomURL = "https://api.openbrewerydb.org/v1/breweries/random";
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
@@ -14,7 +16,7 @@ app.get("/", (req, res)=>{
 
 app.post("/get-random", async(req, res)=>{
     try{
-        const result = await axios.get(URL)
+        const result = await axios.get(randomURL)
         res.render("index.ejs",{
             name: result.data[0].name,
             city: result.data[0].city,
@@ -22,6 +24,33 @@ app.post("/get-random", async(req, res)=>{
             state: result.data[0].state,
             postalCode: result.data[0].postal_code,
             web: result.data[0].website_url
+        })
+    }catch(error){
+        res.render("index.ejs",{
+            name: JSON.stringify(error.response.data),
+            city: JSON.stringify(error.response.data),
+            country: JSON.stringify(error.response.data),
+            state: JSON.stringify(error.response.data),
+            postalCode: JSON.stringify(error.response.data),
+            web: JSON.stringify(error.response.data)
+        })
+    }
+})
+
+app.post("/get-by-state", async(req, res)=>{
+    var state = req.body.state;
+    state = state.toLowerCase().split(' ').join('_')
+    try{
+        const result = await axios.get(`https://api.openbrewerydb.org/v1/breweries?by_state=${state}`)
+        const rng = Math.floor(Math.random() * result.data.length)
+        const resultWithIndex = result.data[rng]
+        res.render("index.ejs", {
+            name: resultWithIndex.name,
+            city: resultWithIndex.city,
+            country: resultWithIndex.country,
+            state: resultWithIndex.state,
+            postalCode: resultWithIndex.postal_code,
+            web: resultWithIndex.website_url
         })
     }catch(error){
         res.render("index.ejs",{
